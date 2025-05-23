338f1fac3603c10e9c42d85d9c7cb0e567d6e16c
CurcoVain <gg.prof.ef@gmail.com>
2025-05-23 20:12:22 -0300

    merge

--- config/config.py
+++ config/config.py
@@ -1,14 +1,20 @@
 from dotenv import load_dotenv
 import os
 
+# Cargar variables del archivo .env
 load_dotenv()
 
-user = os.getenv("MYSQL_USER")
-password = os.getenv ("MYSQL_PASSWORD")
-host = os.getenv("MYSQL_HOST")
-database = os.getenv("MYSQL_DB")
-port= os.getenv("MYSQL_PORT", "3306")
-#verificar el puerto que estes usando en MYSQL
+class Config:
+    SECRET_KEY = os.getenv("SECRET_KEY", "claveporDefecto")
+    
+    # Armamos la URI de conexión a MySQL desde el .env
+    MYSQL_USER = os.getenv("MYSQL_USER")
+    MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+    MYSQL_HOST = os.getenv("MYSQL_HOST")
+    MYSQL_DB = os.getenv("MYSQL_DB")
 
-DATABASE_CONNECTION_URI = f"mysql+pymysql://{user}:{password}@{host}:{port}/{database}"
+    SQLALCHEMY_DATABASE_URI = (
+        f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DB}"
+    )
 
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
