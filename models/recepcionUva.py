bc3a0bb9839c25a4d3a5b0c73b0149f28d51be08
Nahuel <matiasghilardisalinas@gmail.com>
2025-05-23 19:02:03 -0300

    Agregar campos faltantes en modelo recepcionUva.py

--- models/recepcionUva.py
+++ models/recepcionUva.py
@@ -6,17 +6,19 @@ class RecepcionUva(db.Model):
     __tablename__ = 'recepcion_uva'
 
     id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
-    
-    # ForeignKey a LoteVino.id (cadena UUID)
+
+    # FK a loteVino (la columna en la tabla que apunta a la clave primaria de loteVino)
     lote_vino_id = db.Column(db.String(36), db.ForeignKey('lote_vino.id'), nullable=False)
 
     fecha_recepcion = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
-    cantidad_kilos = db.Column(db.Float, nullable=False)
-    proveedor = db.Column(db.String(150), nullable=True)
-    observaciones = db.Column(db.String(300), nullable=True)
+    cantidad_kg = db.Column(db.Float, nullable=False)
+    ph = db.Column(db.Float, nullable=True)
+    acidez_total_g_l = db.Column(db.Float, nullable=True)
+    azucar_brix = db.Column(db.Float, nullable=True)
+    notas = db.Column(db.String(300), nullable=True)
 
-    # Relación para poder acceder al lote desde la recepción
+    # Relación para poder acceder al objeto LoteVino desde un recepcionUva
     lote_vino = db.relationship('LoteVino', backref=db.backref('recepciones', lazy=True))
 
     def __repr__(self):
-        return f"<RecepcionUva {self.id} - Lote {self.lote_vino_id} - {self.cantidad_kilos}kg>"
+        return f"<RecepcionUva {self.id} - Lote {self.lote_vino_id}>"
